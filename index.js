21633c5ffa1026047f5496fabd9c67baa5a9eb9c
John <jschriem@gmail.com>
2020-03-11 13:23:51 -0700

    fixed other snakes

--- index.js
+++ index.js
@@ -52,25 +52,34 @@ app.post('/move', (request, response) => {
       const health = input.you.health;  //snake health
       const food = input.board.food;    //food locations
       const snakess = input.board.snakes;//snake locations
+
+      console.log("initialize variables completed")
       
       const finder = new PF.AStarFinder();
-      const grid = new PF.Grid(width-1, height-1);
+      const grid = new PF.Grid(width, height);
     
+      console.log("initialize grid completed")
+
       //Mark my own body on the grid
       for (let i = 1; i < body.length - 1; i++) {
         grid.setWalkableAt(body[i].x, body[i].y, false);
       }
 
+      console.log("mark on body on grid completed")
+
        //Mark othersnakes on the grid
        for (let i = 1; i < snakess.length - 1; i++) {
-        for (let j = 1; j < snakess.body.length - 1; j++) {
-          grid.setWalkableAt(snakess.body[j].x, snakess.body[j].y, false);
+        for (let j = 1; j < snakess[i].body.length - 1; j++) {
+          grid.setWalkableAt(snakess[i].body[j].x, snakess[i].body[j].y, false);
         }
       }
 
+      console.log("mark other snakes on grid completed")
+
       const data = {}
       var path = finder.findPath(head.x, head.y, food[0].x, food[0].y, grid);
-      console.log(path);
+      
+      console.log("path generation completed " + path);
 
       if (path[1][0] === head.x && path[1][1] === head.y + 1) {
         data.move = 'down'
